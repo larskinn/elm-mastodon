4d1508c825eec941b54f9d07abb19139ebad71ad
Lars Kinn Ekroll <larskinn@gmail.com>
2018-01-28 02:15:20 +0100

    Change Account type to match received data

--- Entities/Account.elm
+++ Entities/Account.elm
@@ -1,18 +1,18 @@
 module Entities.Account exposing (Account, decodeAccount)
 
 import Date exposing (Date)
-import Json.Decode exposing (Decoder, bool, int, nullable, string)
+import Json.Decode as Decode exposing (Decoder, bool, int, nullable, string)
 import Json.Decode.Extra exposing (date)
-import Json.Decode.Pipeline exposing (decode, required)
+import Json.Decode.Pipeline exposing (decode, optional, required)
 
 
 type alias Account =
-    { id : Int
+    { id : String
     , username : String
     , acct : String
     , displayName : String
     , locked : Bool
-    , createdAt : Date -- TODO createdAt should be a date type?
+    , createdAt : Date
     , followersCount : Int
     , followingCount : Int
     , statusesCount : Int
@@ -29,7 +29,7 @@ type alias Account =
 decodeAccount : Decoder Account
 decodeAccount =
     decode Account
-        |> required "id" int
+        |> required "id" string
         |> required "username" string
         |> required "acct" string
         |> required "display_name" string
@@ -44,4 +44,4 @@ decodeAccount =
         |> required "avatar_static" string
         |> required "header" string
         |> required "header_static" string
-        |> required "moved" (nullable string)
+        |> optional "moved" (nullable string) Nothing
