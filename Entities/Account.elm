125fff3cc05eb481bd23abb3b7797bf83b5a3cde
Lars Kinn Ekroll <larskinn@gmail.com>
2018-01-28 02:16:33 +0100

    Add variation of Account type used in verify_account endpoint

--- Entities/Account.elm
+++ Entities/Account.elm
@@ -1,4 +1,4 @@
-module Entities.Account exposing (Account, decodeAccount)
+module Entities.Account exposing (Account, AccountWithSource, decodeAccount, decodeAccountWithSource)
 
 import Date exposing (Date)
 import Json.Decode as Decode exposing (Decoder, bool, int, nullable, string)
@@ -45,3 +45,89 @@ decodeAccount =
         |> required "header" string
         |> required "header_static" string
         |> optional "moved" (nullable string) Nothing
+
+
+type Privacy
+    = Public
+    | Unlisted
+    | Private
+    | Direct
+
+
+decodePrivacy : Decoder Privacy
+decodePrivacy =
+    string
+        |> Decode.andThen
+            (\string ->
+                case string of
+                    "public" ->
+                        Decode.succeed Public
+
+                    "unlisted" ->
+                        Decode.succeed Unlisted
+
+                    "private" ->
+                        Decode.succeed Private
+
+                    "direct" ->
+                        Decode.succeed Direct
+
+                    _ ->
+                        Decode.fail "Invalid Privacy"
+            )
+
+
+type alias Source =
+    { privacy : Privacy
+    , sensitive : Bool
+    , note : String
+    }
+
+
+decodeSource : Decoder Source
+decodeSource =
+    decode Source
+        |> required "privacy" decodePrivacy
+        |> required "sensitive" bool
+        |> required "note" string
+
+
+type alias WithSource a =
+    { a | source : Source }
+
+
+type alias AccountWithSource =
+    WithSource Account
+
+
+mkAccountWithSource : Account -> Source -> AccountWithSource
+mkAccountWithSource account source =
+    { source = source
+    , id = account.id
+    , username = account.username
+    , acct = account.acct
+    , displayName = account.displayName
+    , locked = account.locked
+    , createdAt = account.createdAt
+    , followersCount = account.followersCount
+    , followingCount = account.followingCount
+    , statusesCount = account.statusesCount
+    , note = account.note
+    , url = account.url
+    , avatar = account.avatar
+    , avatarStatic = account.avatarStatic
+    , header = account.header
+    , headerStatic = account.headerStatic
+    , moved = account.moved
+    }
+
+
+decodeAccountWithSource : Decoder AccountWithSource
+decodeAccountWithSource =
+    let
+        decodeWithSource account =
+            Decode.map2 mkAccountWithSource
+                (Decode.succeed account)
+                (Decode.field "source" decodeSource)
+    in
+    decodeAccount |> Decode.andThen decodeWithSource
